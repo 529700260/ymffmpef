f588f7020ee333bd08eb4014c95240358941dcc9
529700260 <39048799+529700260@users.noreply.github.com>
2020-01-13 13:07:57 +0800

    change

--- start.sh
+++ start.sh
@@ -13,13 +13,6 @@ __start() {
   source /etc/profile
    java -jar $JAVA_OPTS   &
   echo "$RUN_NAME started success."
-  #程序名
-  RUN_NAME="$1"
-  #jar 位置
-  JAVA_OPTS=/var/www/"$1".jar
-  source /etc/profile
-   java -jar $JAVA_OPTS   &
-  echo "$RUN_NAME started success."
 }
 
 # Call all functions
