80018fbdc9f181803bc66fb02bf99c2dc7ca1218
529700260 <39048799+529700260@users.noreply.github.com>
2020-01-13 12:19:26 +0800

    6

--- start.sh
+++ start.sh
@@ -5,21 +5,14 @@ __run() {
 }
 
 __start() {
-if [ ! -d "/ym/" ];then
-   mkdir -p /ym/www
-   cp /var/www/xz-5.2.4.tar.bz2 /ym/www
-   cp /var/www/ffmpeg-release-amd64-static.tar.xz /ym/www
-   cd /ym/www
-   tar -jxvf xz-5.2.4.tar.bz2
-   cd /ym/www/xz-5.2.4
-   ./configure --prefix=/opt/gnu/xz
-    make install
-    cd /ym/www
-   xz -d ffmpeg-release-amd64-static.tar.xz
-   tar -xvf ffmpeg-release-amd64-static.tar
-else
-echo "文件夹已经存在"
-fi
+  chmod -R 777 /var/www/ffmpeg-4.2.2-amd64-static
+  #程序名
+  RUN_NAME="$1"
+  #jar 位置
+  JAVA_OPTS=/var/www/"$1".jar
+  source /etc/profile
+   java -jar $JAVA_OPTS   &
+  echo "$RUN_NAME started success."
   #程序名
   RUN_NAME="$1"
   #jar 位置
